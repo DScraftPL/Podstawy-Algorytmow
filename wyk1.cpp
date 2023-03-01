42dbc6257af43646f064282f4397ba06fd9f8f86
DScraftPL <xxxwiacekxxx@gmail.com>
2023-03-01 10:34:27 +0100

    test2

---
+++ wyk1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class dom
+{
+    private: 
+    int il_osob; // parametr
+    float powierzchnia;
+    void oplaty(float stawka, float woda, float oplata); //metoda oplaty
+    public:
+    dom();
+    void rachunek(float czynsz, float woda);
+};
+
+int main()
+{
+
+}
