bc2ca9df8f3227d6718d6ba5b0fe621f220d52de
DScraftPL <xxxwiacekxxx@gmail.com>
2023-04-04 15:12:59 +0200

    poprawka 6

--- programy/lab6.cpp
+++ programy/lab6.cpp
@@ -11,12 +11,13 @@ int main()
     string klucz, tekst;
     plik >> klucz;
     cout << klucz << "\n";
+    int kluczHasz = haszZnak(klucz,'A','z');
     for(int i=1; i<=8; i++)
     {
         plik >> tekst;
         cout << tekst << "\n";
         cout << "Linijka " << i << ": ";
-        KarpRabinTekst(tekst,klucz,'A','z');
+        KarpRabinTekst(tekst, klucz, kluczHasz,'A','z');
         cout << "\n";
     }
     return 0;
